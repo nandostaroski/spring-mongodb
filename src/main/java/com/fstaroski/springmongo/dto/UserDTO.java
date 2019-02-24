78a6c3df5b4da8eddfa3ea2003fe91715932d6ef
FERNANDO STAROSKI <fernnadostaroski@gmail.com>
2019-02-24 11:27:20 -0300

    Criando userDTO e retornando dados

---
+++ src/main/java/com/fstaroski/springmongo/dto/UserDTO.java
@@ -0,0 +1,40 @@
+package com.fstaroski.springmongo.dto;
+
+import java.io.Serializable;
+
+import com.fstaroski.springmongo.domain.User;
+
+public class UserDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private String email;
+
+	public UserDTO(){
+		
+	}
+	
+	public UserDTO(User obj) {
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}

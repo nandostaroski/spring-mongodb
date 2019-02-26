e681ac94321dd2df4f2269aaadc4f05c5b978eee
FERNANDO STAROSKI <fernnadostaroski@gmail.com>
2019-02-26 20:56:51 -0300

    envio de dados do autor com dto para post

--- src/main/java/com/fstaroski/springmongo/config/Instantiation.java
+++ src/main/java/com/fstaroski/springmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fstaroski.springmongo.domain.Post;
 import com.fstaroski.springmongo.domain.User;
+import com.fstaroski.springmongo.dto.AuthorDTO;
 import com.fstaroski.springmongo.repository.PostRepository;
 import com.fstaroski.springmongo.repository.UserRepository;
 
@@ -24,22 +25,22 @@ public class Instantiation implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 
-		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018") , "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018") , "Bom dia", "Acordei feliz hoje!", maria);
-		
-		userRepository.saveAll(Arrays.asList(maria,alex,bob));
-		postRepository.saveAll(Arrays.asList(post1,post2));
-		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
 
 }

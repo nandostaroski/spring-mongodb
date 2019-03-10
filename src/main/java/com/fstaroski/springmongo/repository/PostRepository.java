1ec05003de593f836642583dd02ae8234ca58797
FERNANDO STAROSKI <fernnadostaroski@gmail.com>
2019-03-10 17:09:35 -0300

    Consulta com @Query

--- src/main/java/com/fstaroski/springmongo/repository/PostRepository.java
+++ src/main/java/com/fstaroski/springmongo/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.fstaroski.springmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fstaroski.springmongo.domain.Post;
@@ -11,4 +12,7 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }" )
+    List<Post> findByTitle(String text);
 }

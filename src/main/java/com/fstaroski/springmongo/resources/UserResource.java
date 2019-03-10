9da45314309623aaa8c89480f49b07b3f665a5ee
FERNANDO STAROSKI <fernnadostaroski@gmail.com>
2019-03-10 10:51:21 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/fstaroski/springmongo/resources/UserResource.java
+++ src/main/java/com/fstaroski/springmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fstaroski.springmongo.domain.Post;
 import com.fstaroski.springmongo.domain.User;
 import com.fstaroski.springmongo.dto.UserDTO;
 import com.fstaroski.springmongo.services.UserService;
@@ -58,5 +59,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts",method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
